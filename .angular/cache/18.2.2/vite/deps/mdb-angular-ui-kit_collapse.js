d67e3d91c7b4a3b95835b172dd4f6ccf459bc29a
ngangu63 <banabalukala@gmail.com>
2024-10-10 11:00:44 -0700

    first commit

---
+++ .angular/cache/18.2.2/vite/deps/mdb-angular-ui-kit_collapse.js
@@ -0,0 +1,10 @@
+import {
+  MdbCollapseDirective,
+  MdbCollapseModule
+} from "./chunk-QE3OYUHL.js";
+import "./chunk-FZHMIH5B.js";
+export {
+  MdbCollapseDirective,
+  MdbCollapseModule
+};
+//# sourceMappingURL=mdb-angular-ui-kit_collapse.js.map
