cd211393bbbdca8f5cc6ed233b9dadc52e36a55a
ngangu63 <banabalukala@gmail.com>
2024-10-16 16:01:46 -0700

    pdf1

---
+++ .angular/cache/18.2.2/vite/deps/mdb-angular-ui-kit_collapse.js
@@ -0,0 +1,10 @@
+import {
+  MdbCollapseDirective,
+  MdbCollapseModule
+} from "./chunk-DE2XXRKQ.js";
+import "./chunk-PWQ5RXTN.js";
+export {
+  MdbCollapseDirective,
+  MdbCollapseModule
+};
+//# sourceMappingURL=mdb-angular-ui-kit_collapse.js.map
--- src/app/leadership/leadership.component.ts
+++ src/app/leadership/leadership.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core';
 
 @Component({
   selector: 'app-leadership',
@@ -8,5 +8,54 @@ import { Component } from '@angular/core';
   styleUrl: './leadership.component.scss'
 })
 export class LeadershipComponent {
+  
+  slideIndex = 1;
+  mySlides;
+  fade;
+  dot;
 
+  currentSlide(value: number){
+   this.showSlides(this.slideIndex);
+  }
+
+  showSlides(value: number){
+    let i;
+    //let slides = document.getElementsByClassName("mySlides");
+    this.mySlides
+    
+
+  }
+
+   
 }
+
+
+
+/* let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} */
